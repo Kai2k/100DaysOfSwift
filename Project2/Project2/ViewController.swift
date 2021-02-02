e1ac7fdbc04ae16a095df317a59f53135689a084
Kai Armer <kai.armer@mkodo.com>
2021-02-02 21:22:25 +0000

    Project2 - Randoms and IBAction (day 20)

--- Project2/Project2/ViewController.swift
+++ Project2/Project2/ViewController.swift
@@ -6,23 +6,124 @@ class ViewController: UIViewController {
   @IBOutlet weak var button3: UIButton!
   private var countries = [String]()
   private var score = 0
+  private var tries = 0
+  private var correctAnswer = 0
   
   override func viewDidLoad() {
     super.viewDidLoad()
     countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
-    button1.layer.borderWidth = 1
-    button1.layer.borderColor = UIColor.lightGray.cgColor
-    button2.layer.borderWidth = 1
-    button2.layer.borderColor = UIColor.lightGray.cgColor
-    button3.layer.borderWidth = 1
-    button3.layer.borderColor = UIColor.lightGray.cgColor
-    askQuestion()
+    countries.shuffle()
+    buttonStyles()
+    askQuestion(action: nil)
   }
   
-  private func askQuestion() {
-    button1.setImage(UIImage(named: countries[0]), for: .normal)
-    button2.setImage(UIImage(named: countries[1]), for: .normal)
-    button3.setImage(UIImage(named: countries[2]), for: .normal)
+  private func buttonStyles() {
+    style(button1)
+    style(button2)
+    style(button3)
   }
+  
+  private func style(_ button: UIButton) {
+    button.layer.borderWidth = 1
+    button.layer.borderColor = UIColor.lightGray.cgColor
+  }
+  
+  private func askQuestion(action: UIAlertAction!) {
+    buttonImages()
+    nextCorrectAnswer()
+    appendToTitle(text: solution())
+  }
+  
+  private func buttonImages() {
+    setImageFor(button1, withCountryIndex: 0)
+    setImageFor(button2, withCountryIndex: 1)
+    setImageFor(button3, withCountryIndex: 2)
+  }
+  
+  private func setImageFor(_ button: UIButton, withCountryIndex index: Int) {
+    button.setImage(UIImage(named: countries[index]), for: .normal)
+  }
+  
+  private func nextCorrectAnswer() {
+    correctAnswer = Int.random(in: 0...2)
+  }
+  
+  private func appendToTitle(text: String) {
+    title = text + " - Score: \(score)"
+  }
+  
+  private func solution() -> String {
+    countries[correctAnswer].uppercased()
+  }
+  
+  @IBAction func buttonTapped(_ sender: UIButton) {
+    incrementTries()
+    let outcome = correctButtonMatches(sender) ? answer() : wrongAnswer()
+    appendToTitle(text: outcome.rawValue)
+    if shouldEnd() {
+      end()
+      return
+    }
+    reportTry(outcome)
+  }
+  
+  private func incrementTries() {
+    tries += 1
+  }
+  
+  private func correctButtonMatches(_ sender: UIButton) -> Bool {
+    sender.tag == correctAnswer
+  }
+  
+  private func answer() -> Outcome {
+    incrementScore()
+    return .correct
+  }
+  
+  private func incrementScore() {
+    score += 1
+  }
+  
+  private func wrongAnswer() -> Outcome {
+    decrementScore()
+    return .incorrect
+  }
+  
+  private func decrementScore() {
+    score = max(0, score - 1)
+  }
+  
+  private func shouldEnd() -> Bool {
+    tries >= 10
+  }
+  
+  private func end() {
+    let ac = UIAlertController(title: "Game over", message: "Your final score is \(score).", preferredStyle: .alert)
+    ac.addAction(UIAlertAction(title: "OK", style: .default, handler: quit ))
+    present(ac, animated: true)
+  }
+  
+  private func quit(_ action: UIAlertAction) {
+    exit(1)
+  }
+  
+  private func reportTry(_ outcome: Outcome) {
+    let ac = UIAlertController(title: text(for: outcome), message: "Your score is \(score).", preferredStyle: .alert)
+    ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion ))
+    present(ac, animated: true)
+  }
+  
+  private func text(for outcome: Outcome) -> String {
+    return outcome == .correct ? outcome.rawValue : wrongAnswerText()
+  }
+  
+  private func wrongAnswerText() -> String {
+    "Wrong, the correct answer is \(solution())"
+  }
+}
+
+private enum Outcome: String {
+  case correct = "Correct"
+  case incorrect = "Incorrect"
 }
 
