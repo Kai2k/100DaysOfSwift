431c3a3adb9a42d0f89c164e29afe82c29afef80
Kai Armer <Kai2k@users.noreply.github.com>
2021-07-07 21:54:32 +0100

    Day 32 - Add alert controller

--- Project6c/Project6c/ViewController.swift
+++ Project6c/Project6c/ViewController.swift
@@ -1,12 +1,36 @@
 import UIKit
 
 class ShoppingListViewController: UITableViewController {
+  private var items = [String]()
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    
+  
+    title()
+    barButton()
+  }
+  
+  private func title() {
+    title = "Shopping List"
   }
 
-
+  private func barButton() {
+    navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptToAdd))
+  }
+  
+  @objc private func promptToAdd() {
+    let alertController = UIAlertController(title: "Add", message: nil, preferredStyle: .alert)
+    alertController.addTextField()
+    
+    let submitAction = UIAlertAction(title: "Add item", style: .default) { [weak self, weak alertController] _ in
+      guard let itemText = alertController?.textFields?[0].text else { return }
+    }
+    alertController.addAction(submitAction)
+    present(alertController, animated: true)
+  }
+  
+  private func add(item: String) {
+    items.append(item)
+  }
 }
 
